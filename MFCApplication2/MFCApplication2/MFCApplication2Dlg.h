ccf31c0dbc0d3e78b4310707da44c46e1c245544
Julia-1345 <miroshkina.yuli@gmail.com>
2026-04-22 18:37:40 +0300

    Add files via upload

---
+++ MFCApplication2/MFCApplication2/MFCApplication2Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// MFCApplication2Dlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CMFCApplication2Dlg
+class CMFCApplication2Dlg : public CDialogEx
+{
+// Создание
+public:
+	CMFCApplication2Dlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+	DECLARE_MESSAGE_MAP()
+
+private:
+	COLORREF m_textColor; // цвет текста для статического поля
+
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnStnClickedStaticResult();
+	afx_msg void OnBnClickedButtonFile();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnEnChangeEditQuestion();
+};
